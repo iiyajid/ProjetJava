c5f44e631efd6b3a15131fbeb77dc283ae31b69a
iiyajid <issame.yajid@gmail.com>
2014-11-12 16:01:00 +0100

    obsolete methods

--- AppChat/src/Client.java
+++ AppChat/src/Client.java
@@ -1,45 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
+import java.io.*;
 
+public class Client
+{  private Socket socket              = null;
+   private DataInputStream  console   = null;
+   private DataOutputStream streamOut = null;
 
-public class Client {
-
-	public static void main(String[] args) {
-		String host = "localhost";
-		int portNumber = 1234;
-		try {
-			Socket cliSocket = new Socket(host, portNumber);
-			PrintWriter out = new PrintWriter(cliSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader( new InputStreamReader(cliSocket.getInputStream()));
-			System.out.println("Message reçu: ");
-
-			BufferedReader stdIn =
-	                new BufferedReader(new InputStreamReader(System.in));
-	            String fromServer;
-	            String fromUser;
-
-	            while ((fromServer = in.readLine()) != null) {
-	                System.out.println("Server: " + fromServer);
-	                if (fromServer.equals("Bye."))
-	                    break;
-	                
-	                fromUser = stdIn.readLine();
-	                if (fromUser != null) {
-	                    System.out.println("Client: " + fromUser);
-	                    out.println(fromUser);
-	                }
-	            }
-			
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
+   public Client(String serverName, int serverPort)
+   {  System.out.println("Establishing connection. Please wait ...");
+      try
+      {  socket = new Socket(serverName, serverPort);
+         System.out.println("Connected: " + socket);
+         start();
+      }
+      catch(UnknownHostException uhe)
+      {  System.out.println("Host unknown: " + uhe.getMessage());
+      }
+      catch(IOException ioe)
+      {  System.out.println("Unexpected exception: " + ioe.getMessage());
+      }
+      String line = "";
+      while (!line.equals(".bye"))
+      {  try
+         {  line = console.readLine();
+            streamOut.writeUTF(line);
+            streamOut.flush();
+         }
+         catch(IOException ioe)
+         {  System.out.println("Sending error: " + ioe.getMessage());
+         }
+      }
+   }
+   public void start() throws IOException
+   {  console   = new DataInputStream(System.in);
+      streamOut = new DataOutputStream(socket.getOutputStream());
+   }
+   public void stop()
+   {  try
+      {  if (console   != null)  console.close();
+         if (streamOut != null)  streamOut.close();
+         if (socket    != null)  socket.close();
+      }
+      catch(IOException ioe)
+      {  System.out.println("Error closing ...");
+      }
+   }
+   public static void main(String args[])
+   {  int portNumber = 1234;
+      String hostName = "127.0.0.1";
+	   Client client = new Client(hostName,portNumber);
+   }
 }
--- AppChat/src/Server.java
+++ AppChat/src/Server.java
@@ -1,32 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
 
+public class Server
+{  private Socket          socket   = null;
+   private ServerSocket    server   = null;
+   private DataInputStream streamIn =  null;
 
-public class Server {
-
-	public static void main(String[] args) {
-		String data;
-		int portNumber = 1234;
-		try {
-			ServerSocket serverSocket = new ServerSocket(portNumber);
-			Socket clientSocket = serverSocket.accept();
-			System.out.println("Serveur connecté");
-			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			while( !(data = in.readLine()).isEmpty())
-				out.println(data);
-			
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-
-	}
-
+   public Server(int port)
+   {  try
+      {  System.out.println("Binding to port " + port + ", please wait  ...");
+         server = new ServerSocket(port);  
+         System.out.println("Server started: " + server);
+         System.out.println("Waiting for a client ..."); 
+         socket = server.accept();
+         System.out.println("Client accepted: " + socket);
+         open();
+         boolean done = false;
+         while (!done)
+         {  try
+            {  String line = streamIn.readUTF();
+               System.out.println(line);
+               done = line.equals(".bye");
+            }
+            catch(IOException ioe)
+            {  done = true;
+            }
+         }
+         close();
+      }
+      catch(IOException ioe)
+      {  System.out.println(ioe); 
+      }
+   }
+   public void open() throws IOException
+   {  streamIn = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+   }
+   public void close() throws IOException
+   {  if (socket != null)    socket.close();
+      if (streamIn != null)  streamIn.close();
+   }
+   public static void main(String args[])
+   {  int portNumber = 1234;
+      Server server = new Server(portNumber);
+//      if (args.length != 1)
+//         System.out.println("Usage: java ChatServer port");
+//      else
+//         server = new Server(Integer.parseInt(args[0]));
+   }
 }
