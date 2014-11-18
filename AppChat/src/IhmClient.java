081a4240cf4946ac96b9fc2ca0303c01f68f9cc5
iiyajid <issame.yajid@gmail.com>
2014-11-18 15:10:23 +0100

    ihm

--- AppChat/src/IhmClient.java
+++ AppChat/src/IhmClient.java
@@ -1,6 +1,7 @@
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 
+import javax.print.attribute.standard.Severity;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,7 +10,7 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 
-public class IhmClient extends JFrame implements ActionListener {
+public class IhmClient extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JTextField msgField, serverAddress, serverPort;
 	private JButton loginBtn, logoutBtn, whoIsInBtn;
@@ -25,13 +26,30 @@ public class IhmClient extends JFrame implements ActionListener {
 		defAddress = address;
 		defPort = port;
 		
-		JPanel haut = new JPanel(new GridLayout(3,1));
-		JPanel bas = new JPanel(new GridLayout(1,5, 1, 3));
+		setTitle("Client");
+		setSize(700,500); 
+		setLocation(50,200); 
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new GridLayout(2, 1));
+		
+		serverAddress.setText("127.0.0.1");
+		serverPort.setText("1234");
+		
+		JPanel pConnect = new JPanel(new GridLayout(2,1));
+		pConnect.add(serverAddress);
+		pConnect.add(serverPort);
+		JPanel pChat = new JPanel(new GridLayout(1,1));
+		
+		this.add(pConnect);
 		
 		serverAddress = new JTextField(address);
 		serverPort = new JTextField(port);
 		serverPort.setHorizontalAlignment(SwingConstants.RIGHT);
 	}
 	
+	 public static void main (String [] args) {
+		 IhmClient cli = new IhmClient("127.0.0.1", 1234);
+	 }
 
 }
